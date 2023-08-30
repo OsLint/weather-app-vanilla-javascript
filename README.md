1d01972d44dd280d69a6c3ab1d8ddee7f687c6fd
oskalbarczyk <oskalbarczyk@gmail.com>
2023-08-30 17:37:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Weather App README 🌦️
+
+The Weather App is a simple web application that allows users to search for weather information of a specific city. It fetches weather data from the OpenWeatherMap API based on the user's input and displays the relevant weather details on the user interface.
+
+## Features ⭐
+
+- **Search Functionality:** Users can enter the name of a city in the search input field and click the search button to retrieve weather information for that city.
+- **Weather Display:** Once a valid city name is entered and the search button is clicked, the app displays the current weather information for the specified city. This includes the city name, temperature, humidity, and wind speed. The app also displays an appropriate weather icon and adjusts the background color based on the weather condition.
+- **Error Handling:** If the entered city name is not found in the OpenWeatherMap API, an error message is displayed to inform the user of the invalid input.
+
+## Technologies Used 🛠️
+
+- HTML: Provides the structure of the web page.
+- CSS: Defines the styling and layout of the web page.
+- JavaScript: Implements the functionality of fetching weather data and updating the user interface.
+- OpenWeatherMap API: Provides weather data based on the city name.
+
+## How to Use 🚀
+
+1. Open the `index.html` file in a web browser.
+2. In the search input field, enter the name of the city for which you want to check the weather.
+3. Click the search button (🔍 icon) next to the input field.
+4. If the city name is valid and weather data is available, the app will display the current weather information, including temperature, humidity, wind speed, and an appropriate weather icon.
+5. If the entered city name is not found, an error message will be displayed.
+
+![screanshot](https://github.com/oskalbarczyk/weather-app-vanilla-javascript/assets/106467480/648e42ee-7e9d-46a2-bf4a-13f7274732ca)
+
+
+
+## Installation 💻
+
+Since the Weather App is a client-side web application, there is no installation required. Simply download the provided files (`index.html`, `styles.css`, `app.js`, and the `images` folder) and open the `index.html` file in a web browser to use the app.
+
+## Credits 💡
+
+- Weather data is provided by the [OpenWeatherMap API](https://openweathermap.org/).
+- Icons used in the app are provided by the [Flaticon](https://www.flaticon.com/) website.
+
+## License 📄
+
+This Weather App is provided under the [MIT License](LICENSE).
+
+---
+
+Feel free to customize and enhance the Weather App as needed for your use case! 🌈
---
+++ app.js
@@ -0,0 +1,86 @@
+//Add your ApiKey from openweathermap.org
+const apiKey;
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const card = document.querySelector(".card");
+const cloudAnimation = document.querySelector(".container");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    return;
+  } else {
+    restartFadeInAnimation();
+    cloudAnimation.style.display = "none";
+    var data = await response.json();
+
+    // console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    switch (data.weather[0].main) {
+      case "Clouds":
+        weatherIcon.src = "images/clouds.png";
+        card.style.background = "linear-gradient(135deg, #b0c4b1, #6e7f74)";
+        card.style.boxShadow = "0px 4px 15px  #b0c4b1";
+        break;
+      case "Clear":
+        weatherIcon.src = "images/clear.png";
+        card.style.background = "linear-gradient(135deg, #ff6b6b, #ffb347)";
+        card.style.boxShadow = "0px 4px 15px  #ff6b6b";
+        break;
+      case "Rain":
+        weatherIcon.src = "images/rain.png";
+        card.style.background = "linear-gradient(135deg, #2f3d4c, #644e58)";
+        card.style.boxShadow = "0px 4px 15px  #2f3d4c";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "images/drizzle.png";
+        card.style.background = "linear-gradient(135deg, #b0c4b1, #6e7f74)";
+        card.style.boxShadow = "0px 4px 15px   #b0c4b1";
+        break;
+      case "Mist":
+        weatherIcon.src = "images/mist.png";
+        card.style.background = "linear-gradient(135deg, #3a3f44, #a4b8d9)";
+        card.style.boxShadow = "0px 4px 15px  #3a3f44";
+        break;
+      default:
+        throw new Error("Invalid weather data");
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+function restartFadeInAnimation() {
+  // Remove animation properties
+  document.body.style.animation = "none";
+
+  // Trigger a reflow to apply the style changes immediately
+  void document.body.offsetWidth;
+
+  // Add animation properties back
+  document.body.style.animation = "fadeInAnimation ease 3s";
+  document.body.style.animationIterationCount = "1";
+  document.body.style.animationFillMode = "forwards";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+searchBox.addEventListener("keyup", function (event) {
+  //after pressing enter call checkWeather function
+  if (event.keyCode == 13) {
+    checkWeather(searchBox.value);
+  }
+});
