d935ee3e107e5f96d66fc080e714692ea5ba44aa
oskalbarczyk <106467480+oskalbarczyk@users.noreply.github.com>
2024-03-12 08:50:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Add your ApiKey from openweathermap.org
 const apiKey;
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+ 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
